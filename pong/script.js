0bbee9b8cdbecd088b1eb08234d1fe581116cc3c
s3m <s3m@archlinux.org>
2023-03-01 10:53:28 +0100

    added paddle-ball collisions and added drawtext function

--- pong/script.js
+++ pong/script.js
@@ -3,10 +3,9 @@ const ctx = canvas.getContext("2d")
 const fps = 60
 
 document.addEventListener("keydown", event => {
-    console.log(event.keyCode)
-    if (event.keyCode == 188) {
+    if (event.keyCode == 87) {
         paddle1.yvelocity = -4
-    } else if (event.keyCode == 79) {
+    } else if (event.keyCode == 83) {
         paddle1.yvelocity = 4
     } 
     if (event.keyCode == 38) {
@@ -17,10 +16,9 @@ document.addEventListener("keydown", event => {
 })
 
 document.addEventListener("keyup", event => {
-    console.log(event.keyCode)
-    if (event.keyCode == 188) {
+    if (event.keyCode == 87) {
         paddle1.yvelocity = 0
-    } else if (event.keyCode == 79) {
+    } else if (event.keyCode == 83) {
         paddle1.yvelocity = 0
     } 
     if (event.keyCode == 38) {
@@ -31,8 +29,8 @@ document.addEventListener("keyup", event => {
 })
 
 const ball = {
-    x: 0,
-    y: 0,
+    x: canvas.width/2-25/2,
+    y: canvas.height/2-25/2,
     size: 25,
     xvelocity: 2,
     yvelocity: 2,
@@ -44,6 +42,7 @@ const paddle1 = {
     width: 25,
     height: 100,
     yvelocity: 0,
+    score: 0,
 }
 
 const paddle2 = {
@@ -52,6 +51,7 @@ const paddle2 = {
     width: 25,
     height: 100,
     yvelocity: 0,
+    score: 0,
 }
 
 function drawRect(x, y, w, h, hex) {
@@ -59,17 +59,43 @@ function drawRect(x, y, w, h, hex) {
     ctx.fillRect(x, y, w, h)
 }
 
+function drawText(x, y, text, size, hex) {
+    ctx.fillStyle = hex
+    ctx.font = `${size}px sans-serif`
+    ctx.fillText(text, x, y)
+}
+
 function ballMovement() {
     ball.x += ball.xvelocity
     ball.y += ball.yvelocity
 
-    if (ball.x <= 0) {
+    if (ball.x + ball.xvelocity <= (paddle1.x + paddle1.width) &&
+        ball.y + ball.yvelocity <= (paddle1.y + paddle1.height) &&
+        (ball.x + ball.size) + ball.xvelocity >= paddle1.x &&
+        (ball.y + ball.size) + ball.yvelocity >= paddle1.y) {
         ball.xvelocity *= -1
-    } else if (ball.y <= 0) {
+    }
+
+    if (ball.x + ball.xvelocity <= (paddle2.x + paddle2.width) &&
+        ball.y + ball.yvelocity <= (paddle2.y + paddle2.height) &&
+        (ball.x + ball.size) + ball.xvelocity >= paddle2.x &&
+        (ball.y + ball.size) + ball.yvelocity >= paddle2.y) {
+        ball.xvelocity *= -1
+    }
+
+    if (ball.x + ball.xvelocity <= 0 + paddle1.width) {
+        paddle2.score += 1
+        ball.x = canvas.width/2-ball.size/2
+        ball.y = canvas.height/2-ball.size/2
+    } else if ((ball.x + ball.size) + ball.xvelocity >= canvas.width - paddle2.width) {
+        paddle1.score += 1
+        ball.x = canvas.width/2-ball.size/2
+        ball.y = canvas.height/2-ball.size/2
+    }
+
+    if (ball.y + ball.yvelocity <= 0) {
         ball.yvelocity *= -1
-    } else if (ball.x + ball.size >= canvas.width) {
-        ball.xvelocity *= -1
-    } else if (ball.y + ball.size >= canvas.height) {
+    } else if ((ball.y + ball.size) + ball.xvelocity >= canvas.height) {
         ball.yvelocity *= -1
     }
 }
@@ -77,6 +103,18 @@ function ballMovement() {
 function paddleMovement() {
     paddle1.y += paddle1.yvelocity
     paddle2.y += paddle2.yvelocity
+
+    if (paddle1.y < 0) {
+        paddle1.y = 0
+    } else if (paddle1.y + paddle1.height > canvas.height) {
+        paddle1.y = canvas.height-paddle1.height
+    }
+
+    if (paddle2.y < 0) {
+        paddle2.y = 0
+    } else if (paddle2.y + paddle2.height > canvas.height) {
+        paddle2.y = canvas.height-paddle2.height
+    }
 }
 
 function main() {
